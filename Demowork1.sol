5e3abc27998607cda0b1c020b9a9e071063fb9d2
Srivigneshwaran <68459551+vigneshvicky08@users.noreply.github.com>
2022-08-15 15:07:33 +0530

    Update Demowork1.sol

--- Demowork1.sol
+++ Demowork1.sol
@@ -1,1 +1,12 @@
-cefefcfcecfcecefff
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC20/ERC20.sol)
+
+pragma solidity ^0.8.7;
+
+import "./Demowork2.sol";
+
+contract Demotoken is Demowork2 {
+    constructor() Demowork2("Demotoken","DMT") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+}
